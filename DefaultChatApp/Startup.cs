e36b29e79ccd10ddad88c6715a44c8cf0dc53cf2
Sebastian Udden <sebastian.udden@gmail.com>
2016-04-01 13:22:51 +0200

    Add project files.

---
+++ DefaultChatApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DefaultChatApp.Startup))]
+
+namespace DefaultChatApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
